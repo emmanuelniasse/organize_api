8815aa6f9ce77674117d78ce7a2e0111313a17e1
emmanuelniasse <emmanuelniasse@gmail.com>
2023-10-22 00:06:23 +0200

    vinejs - contrôle des données insérées

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,6 +3,7 @@ import { success, error } from '../functions/functions.js';
 import { ObjectId } from 'mongodb';
 import * as bcrypt from 'bcrypt';
 import Users from '../schemas/usersSchema.js';
+import vine, { SimpleMessagesProvider } from '@vinejs/vine';
 
 const usersRouter = Router();
 
@@ -38,26 +39,47 @@ usersRouter
             const { pseudo, password } = req.body;
             const saltRounds = 10;
 
-            // Vérifie si l'utilisateur est déjà créé
-            const userExist = await Users.findOne({ pseudo });
-            if (userExist && userExist._id != req.params.id) {
-                throw new Error('Utilisateur déjà créé');
-            }
+            const schema = vine.object({
+                pseudo: vine.string(),
+                password: vine.string().minLength(2).maxLength(32),
+            });
 
-            bcrypt.hash(
+            const data = {
+                pseudo,
                 password,
-                saltRounds,
-                async function (err, password) {
-                    // Créer un nouvel utilisateur
-                    // Pour tester la req avec postman, envoyer les datas via body > raw > JSON (à cause du `required: true` dans le schema)
-                    const newUser = new Users({
-                        pseudo,
-                        password,
-                    });
-                    const userCreated = await newUser.save();
-                    res.status(200).json(success(userCreated));
+            };
+
+            const output = await vine.validate({
+                schema,
+                data,
+            });
+
+            if (output) {
+                // Vérifie si l'utilisateur est déjà créé
+                const userExist = await Users.findOne({ pseudo });
+                if (userExist) {
+                    throw new Error('Utilisateur déjà créé');
                 }
-            );
+
+                // Crée l'utilisateur
+                bcrypt.hash(
+                    password,
+                    saltRounds,
+                    async function (err, password) {
+                        if (err) {
+                            res.status(400).json({
+                                message: 'Utilisateur déjà créé',
+                            });
+                        }
+                        const newUser = new Users({
+                            pseudo,
+                            password,
+                        });
+                        const userCreated = await newUser.save();
+                        res.status(200).json(success(userCreated));
+                    }
+                );
+            }
         } catch (err) {
             res.status(500).json(error(err.message));
         }
@@ -68,15 +90,35 @@ usersRouter
         try {
             const { pseudo, password } = req.body;
 
-            const user = await Users.findOne({ pseudo });
+            const schema = vine.object({
+                pseudo: vine.string(),
+                password: vine.string().minLength(2).maxLength(32),
+            });
 
-            bcrypt.compare(
+            const data = {
+                pseudo,
                 password,
-                user.password,
-                function (err, result) {
-                    result && res.status(200).json(success(result));
-                }
-            );
+            };
+
+            const output = await vine.validate({
+                schema,
+                data,
+            });
+
+            if (output) {
+                const user = await Users.findOne({ pseudo });
+
+                bcrypt.compare(
+                    password,
+                    user.password,
+                    function (err, result) {
+                        if (result) {
+                            res.status(200).json(success(result));
+                        }
+                        err && console.log(err);
+                    }
+                );
+            }
         } catch (err) {
             console.log(err);
         }
