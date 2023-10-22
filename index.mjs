9c8c449a1c855d3adcbd8464bd144193569ff539
emmanuelniasse <emmanuelniasse@gmail.com>
2023-10-22 19:10:26 +0200

    corsOptions

--- index.mjs
+++ index.mjs
@@ -14,7 +14,11 @@ import { usersRouter } from './router/usersRouter.js';
 import { expensesRouter } from './router/expensesRouter.js';
 import { categoriesRouter } from './router/categoriesRouter.js';
 
-app.use(cors());
+const corsOptions = {
+    origin: 'https://organize-kappa.vercel.app/',
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router); // Uniquement "/" car tout est géré dans les routers
