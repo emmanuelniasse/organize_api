3c7730041a22141d8cc540e37ac005ed5cae389e
emmanuelniasse <emmanuelniasse@gmail.com>
2023-11-04 00:46:44 +0100

    Access-Control-Allow-Origin

--- index.mjs
+++ index.mjs
@@ -48,12 +48,6 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.use(
-    cors({
-        origin: '*',
-        credentials: true,
-    })
-);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
