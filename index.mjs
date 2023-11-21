183830ddf750fb0d0d1fb6f1295f2f13c7d2edcb
emmanuelniasse <emmanuelniasse@gmail.com>
2023-11-21 12:22:00 +0100

    origin app-url 5

--- index.mjs
+++ index.mjs
@@ -24,7 +24,7 @@ app.use(
     cors({
         // origin: process.env.APP_URL_PROD,
         // origin: '*',
-        origin: 'https://organize-kappa.vercel.app/',
+        origin: 'https://organize-kappa.vercel.app',
         methods: 'GET,POST,PUT,DELETE,OPTIONS',
         allowedHeaders:
             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, ngrok-skip-browser-warning, Authorization',
