6c8f350613466ec4f8cad5acc7e8cb77fdb855fe
emmanuelniasse <emmanuelniasse@gmail.com>
2023-12-05 23:50:58 +0100

    single middleware

--- index.mjs
+++ index.mjs
@@ -24,8 +24,28 @@ app.use(
     cors({
         origin: 'https://organize-kappa.vercel.app',
         credentials: true, // pour envoyer les cookies au client
+        allowedHeaders: ['Content-Type', 'Authorization'], // pour autoriser le header Content-Type et Authorization dans les requêtes envoyées au serveur (pour les cookies)
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // pour autoriser les requêtes GET, POST, PUT, DELETE depuis le client
         })
 );
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Authorization', 'Content-Type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router); // Uniquement "/" car tout est géré dans les routers
