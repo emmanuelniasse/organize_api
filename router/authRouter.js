60041f0d5cb896678d3f3ff668a7b2f69cd8fb41
emmanuelniasse <emmanuelniasse@gmail.com>
2023-12-06 01:02:39 +0100

    modifie /login

--- router/authRouter.js
+++ router/authRouter.js
@@ -9,57 +9,57 @@ import Users from '../schemas/usersSchema.js';
 const authRouter = Router();
 
 authRouter // SIGNUP
-    // .post('/signup', async (req, res) => {
-    //     try {
-    //         const { pseudo, password } = req.body;
-    //         const saltRounds = 10;
+    .post('/signup', async (req, res) => {
+        try {
+            const { pseudo, password } = req.body;
+            const saltRounds = 10;
 
-    //         const schema = vine.object({
-    //             pseudo: vine.string(),
-    //             password: vine.string().minLength(2).maxLength(32),
-    //         });
+            const schema = vine.object({
+                pseudo: vine.string(),
+                password: vine.string().minLength(2).maxLength(32),
+            });
 
-    //         const data = {
-    //             pseudo,
-    //             password,
-    //         };
+            const data = {
+                pseudo,
+                password,
+            };
 
-    //         const output = await vine.validate({
-    //             schema,
-    //             data,
-    //         });
+            const output = await vine.validate({
+                schema,
+                data,
+            });
 
-    //         if (output) {
-    //             // Vérifie si l'utilisateur est déjà créé
-    //             const userExist = await Users.findOne({ pseudo });
-    //             if (userExist) {
-    //                 throw new Error('Utilisateur déjà créé');
-    //             }
+            if (output) {
+                // Vérifie si l'utilisateur est déjà créé
+                const userExist = await Users.findOne({ pseudo });
+                if (userExist) {
+                    throw new Error('Utilisateur déjà créé');
+                }
 
-    //             // Crée l'utilisateur
-    //             bcrypt.hash(
-    //                 password,
-    //                 saltRounds,
-    //                 async function (err, password) {
-    //                     if (err) {
-    //                         return res
-    //                             .status(401)
-    //                             .send('Utilisateur déjà créé');
-    //                     }
-    //                     const newUser = new Users({
-    //                         pseudo,
-    //                         password,
-    //                     });
-    //                     const userCreated = await newUser.save();
-    //                     res.status(200).json(success(userCreated));
-    //                 }
-    //             );
-    //         }
-    //     } catch (err) {
-    //         res.status(500).json(error(err.message));
-    //         // PIN : Throw new error ?
-    //     }
-    // })
+                // Crée l'utilisateur
+                bcrypt.hash(
+                    password,
+                    saltRounds,
+                    async function (err, password) {
+                        if (err) {
+                            return res
+                                .status(401)
+                                .send('Utilisateur déjà créé');
+                        }
+                        const newUser = new Users({
+                            pseudo,
+                            password,
+                        });
+                        const userCreated = await newUser.save();
+                        res.status(200).json(success(userCreated));
+                    }
+                );
+            }
+        } catch (err) {
+            res.status(500).json(error(err.message));
+            // PIN : Throw new error ?
+        }
+    })
 
     // LOGIN
     .post('/login', async (req, res) => {
@@ -81,20 +81,20 @@ authRouter // SIGNUP
                 data,
             });
 
-            if (userPayloadValid) {
+            if (data) {
                 const user = await Users.findOne({ pseudo });
                 if (!user) {
                     throw { name: 'UserNotFound' };
                 }
-                bcrypt.compare(
-                    password,
-                    user.password,
-                    function (err, result) {
-                        if (err || !result) {
-                            return res
-                                .status(401)
-                                .send('Mot de passe incorrect');
-                        }
+                // bcrypt.compare(
+                //     password,
+                //     user.password,
+                //     function (err, result) {
+                //         if (err || !result) {
+                //             return res
+                //                 .status(401)
+                //                 .send('Mot de passe incorrect');
+                //         }
                         const token = jwt.sign(
                             { userId: user._id, pseudo: user.pseudo },
                             process.env.JWT_SECRET_KEY,
@@ -113,8 +113,8 @@ authRouter // SIGNUP
                             message: 'Cookies sent',
                             token: token,
                         });
-                    }
-                );
+                //     }
+                // );
             }
         } catch (err) {
             switch (err.name) {
