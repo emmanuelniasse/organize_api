923ee02d7adf730a4d8250e952c119b6dcfb4ecb
emmanuelniasse <emmanuelniasse@gmail.com>
2023-12-07 01:31:31 +0100

    credentials

--- index.mjs
+++ index.mjs
@@ -20,7 +20,7 @@ import { authVerification } from './router/authVerification.js';
 
 app.use(cookieParser());
 
-app.use(cors({origin: `${process.env.APP_URL}`, allowedHeaders: ['Content-Type', 'Authorization']}));
+app.use(cors({origin: `${process.env.APP_URL}`, allowedHeaders: ['Content-Type', 'Authorization'], credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router); // Uniquement "/" car tout est géré dans les routers
