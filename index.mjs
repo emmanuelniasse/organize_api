09fa04db3c119eeb2123ee4b11fe8401172c7fea
emmanuelniasse <emmanuelniasse@gmail.com>
2023-12-12 19:29:55 +0100

    crendentials include

--- index.mjs
+++ index.mjs
@@ -24,7 +24,7 @@ app.use(cors({
 
     origin: `${process.env.APP_URL}`, 
     allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, 
+    credentials: 'include', 
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express.json());
