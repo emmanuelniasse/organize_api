ab32ebf1e7f2efe37e885ef9d8a1db9233d1ee3c
emmanuelniasse <emmanuelniasse@gmail.com>
2023-12-12 19:15:28 +0100

    tst

--- router/authRouter.js
+++ router/authRouter.js
@@ -105,7 +105,9 @@ authRouter // SIGNUP
                         res.cookie('token', 'tst', {
                                 httpOnly: false,
                                 credentials: true,
-                            });
+                            })
+                            .status(200)
+                            .json(success({ token }));
 
                         // res.send({
                         //     success: true,
