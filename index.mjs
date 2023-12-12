eeb528ac33437d45caee1f75c252768f2bacc4c0
emmanuelniasse <emmanuelniasse@gmail.com>
2023-12-12 17:43:43 +0100

    adding slash

--- index.mjs
+++ index.mjs
@@ -21,7 +21,8 @@ import { authVerification } from './router/authVerification.js';
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'https://organize-kappa.vercel.app', 
+
+    origin: 'https://organize-kappa.vercel.app/', 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE']
