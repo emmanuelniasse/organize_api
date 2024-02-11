4dc24d0c814b9ef94ab8ed1180733a1c504f62a6
emmanuelniasse <emmanuelniasse@gmail.com>
2024-02-11 13:54:57 +0100

    cors: all

--- index.mjs
+++ index.mjs
@@ -1,41 +1,44 @@
 // LIBRAIRIES
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
 
 const app = express();
 const router = express.Router();
 
 // DATABASE Connexion
-import * as dbConnect from './router/dbConnect/dbConnect.js';
+import * as dbConnect from "./router/dbConnect/dbConnect.js";
 dbConnect.connect();
 
 // ROUTERS
-import { authRouter } from './router/authRouter.js';
-import { usersRouter } from './router/usersRouter.js';
-import { expensesRouter } from './router/expensesRouter.js';
-import { categoriesRouter } from './router/categoriesRouter.js';
-import { authVerification } from './router/authVerification.js';
+import { authRouter } from "./router/authRouter.js";
+import { authVerification } from "./router/authVerification.js";
+import { categoriesRouter } from "./router/categoriesRouter.js";
+import { expensesRouter } from "./router/expensesRouter.js";
+import { usersRouter } from "./router/usersRouter.js";
 
 app.use(cookieParser());
-app.use(cors({
-    origin: `${process.env.APP_URL}`, 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+app.use(
+    cors({
+        // origin: `${process.env.APP_URL}`,
+        origin: "*",
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router); // Uniquement "/" car tout est géré dans les routers
-app.use('/', usersRouter);
-app.use('/', authRouter);
-app.use('/', authVerification, expensesRouter);
-app.use('/', authVerification, categoriesRouter);
+app.use("/", usersRouter);
+app.use("/", authRouter);
+app.use("/", authVerification, expensesRouter);
+app.use("/", authVerification, categoriesRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log('Server listening on port ' + process.env.PORT);
+    console.log("Server listening on port " + process.env.PORT);
 });
 
 // Export Express API
